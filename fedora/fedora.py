4065f8e2c1360002383560ab3b0fae58f3ef3b28
Mark Baggett <mbagget1@utk.edu>
2021-03-08 15:50:40 -0500

    Don't assume that object is not a literal.

--- fedora/fedora.py
+++ fedora/fedora.py
@@ -28,7 +28,7 @@ class FedoraObject:
 
     def add_relationship(self, pid, subject, predicate, obj, is_literal="true"):
         r = requests.post(
-            f"{self.fedora_url}/fedora/objects/{pid}/relationships/new?subject={quote(subject, safe='')}&predicate={quote(predicate, safe='')}&object={quote(obj, safe='')}&isLiteral=false",
+            f"{self.fedora_url}/fedora/objects/{pid}/relationships/new?subject={quote(subject, safe='')}&predicate={quote(predicate, safe='')}&object={quote(obj, safe='')}&isLiteral={is_literal}",
             auth=self.auth,
         )
         if r.status_code == 200:
@@ -37,6 +37,17 @@ class FedoraObject:
             raise Exception(
                 f"Unable to add relationship on {pid} with subject={subject}, predicate={predicate}, and object={obj}, and isLiteral as {is_literal}.  Returned {r.status_code}"
             )
+
+    def create_digital_object(self):
+        # Ingest a new object
+        # Add that object to a collection
+        # Give it a content model
+        # Version relationships
+        # Add the AIP
+        # Add the DIP
+        # Give it metadata
+        # Transform DublinCore
+        return
 
 
 if __name__ == "__main__":
