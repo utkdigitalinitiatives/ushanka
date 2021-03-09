b42b9148cd0c2b52a79046c2d4e10d5149420cf5
Mark Baggett <mbagget1@utk.edu>
2021-03-09 13:20:05 -0500

    Add add datastream.

--- fedora/fedora.py
+++ fedora/fedora.py
@@ -1,5 +1,6 @@
 import requests
 from urllib.parse import quote
+import magic
 
 
 class FedoraObject:
@@ -82,6 +83,66 @@ class FedoraObject:
                 f"Unable to change versioning of the {dsid} datastream on {pid} to {versionable}.  Returned {r.status_code}."
             )
 
+    def add_managed_datastream(
+        self,
+        pid,
+        dsid,
+        file,
+        versionable="true",
+        datastream_state="A",
+        checksum_type="DEFAULT",
+    ):
+        """Adds an internally managed datastream.
+
+        This is not a one to one vesion of addDatastream.  It has been stripped down to fit one use case: internally
+        managed content.  I'll add other methods if other use cases arise.
+
+        Args:
+            pid (str): The persistent identifier to the object when you want to add a file.
+            dsid (str): The datastream id to assign your new file.
+            file (str): The path to your file.
+            versionable (str): Defaults to "true".  Specifies whether the datastream should have versioning ("true" or "false").
+            datastream_state (str): Specify whether the datastream is active, inactive, or deleted.
+            checksum_type (str): The checksum type to use.  Defaults to "DEFAULT". See API docs for options.
+
+        Returns:
+            int: The http status code of the request.
+
+        Examples:
+            >>> FedoraObject().add_managed_datastream("test:10", "AIP", "my_aip.7z")
+            201
+
+        """
+        if checksum_type not in (
+            "DEFAULT",
+            "DISABLED",
+            "MD5",
+            "SHA-1",
+            "SHA-256",
+            "SHA-385",
+            "SHA-512",
+        ):
+            raise Exception(
+                f"\nInvalid checksum type specified for {pid} when adding the {dsid} datastream with {file}"
+                f"content.\nMust be one of: DEFAULT, DISABLED, MD5, SHA-1, SHA-256, SHA-385, SHA-512."
+            )
+        mime = magic.Magic(mime=True)
+        upload_file = {
+            "file": (file, open(file, "rb"), mime.from_file(file), {"Expires": "0"})
+        }
+        r = requests.post(
+            f"{self.fedora_url}/fedora/objects/{pid}/datastreams/{dsid}/?controlGroup=M&dsLabel={dsid}&versionable={versionable}&dsState={datastream_state}&checksumType={checksum_type}",
+            auth=self.auth,
+            files=upload_file,
+        )
+        if r.status_code == 201:
+            return r.status_code
+        else:
+            raise Exception(
+                f"\nFailed to create {dsid} datastream on {pid} with {file} as content. Fedora returned this"
+                f"status code: {r.status_code}."
+            )
+
     def create_digital_object(
         self, object_namespace, object_label, collection, object_state="A"
     ):
@@ -114,6 +175,11 @@ class FedoraObject:
 
 
 if __name__ == "__main__":
-    x = FedoraObject().create_digital_object(
-        "test", "Mark's New Test", "islandora:test"
+    # x = FedoraObject().create_digital_object(
+    #     "test", "Mark's New Test", "islandora:test"
+    # )
+    x = FedoraObject().add_managed_datastream(
+        "test:10",
+        "AIP",
+        file="Chronicling_COVID-19-20210215T185151Z-001-2aaa349a-12a2-4338-90d1-5097bb989acc.7z",
     )
